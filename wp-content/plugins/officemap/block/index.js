9972b335fd403989d90f53f0c9e35024908f743d
Yasuaki Kudo <yasu@yasuaki.com>
2025-08-19 20:06:59 +0900

    i

--- wp-content/plugins/officemap/block/index.js
+++ wp-content/plugins/officemap/block/index.js
@@ -1,22 +1,21 @@
-(function (wp) {
-  const { registerBlockType } = wp.blocks;
-  const { __ } = wp.i18n;
-  const ServerSideRender = wp.serverSideRender;
-  const el = wp.element.createElement;
+// WP 6.5+ Script Module (ESM)
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+import ServerSideRender from '@wordpress/server-side-render';
+import { createElement as el } from '@wordpress/element';
 
-  registerBlockType('officemap/nonce', {
-    title: __('OfficeMap (Nonce Starter)', 'officemap'),
-    icon: 'location',
-    category: 'widgets',
+registerBlockType('officemap/nonce', {
+  title: __('OfficeMap (Nonce Starter)', 'officemap'),
+  icon: 'location',
+  category: 'widgets',
 
-    // Show the PHP-rendered markup inside the editor.
-    edit: (props) =>
-      el(ServerSideRender, {
-        block: 'officemap/nonce',
-        attributes: props.attributes,
-      }),
+  // Show the PHP-rendered markup inside the editor.
+  edit: (props) =>
+    el(ServerSideRender, {
+      block: 'officemap/nonce',
+      attributes: props.attributes,
+    }),
 
-    // Dynamic block is rendered by PHP on the frontend.
-    save: () => null,
-  });
-})(window.wp);
+  // Dynamic block is rendered by PHP on the frontend.
+  save: () => null,
+});
--- wp-content/plugins/officemap/block/view.asset.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-  'dependencies' => [ 'wp-element', 'wp-dom-ready' ],
-  'version' => filemtime(__DIR__ . '/view.js'),
-];
