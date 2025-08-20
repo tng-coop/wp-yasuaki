ae99e1a11e710e80630e211f43cab4f99cf6cfe1
Yasuaki Kudo <yasu@yasuaki.com>
2025-08-21 08:33:09 +0900

    i

--- wp-content/plugins/fullshow/render.php
+++ wp-content/plugins/fullshow/render.php
@@ -13,3 +13,22 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php endfor; ?>
 	</div>
 </div>
+
+<script type="module">
+  import Split from 'https://esm.sh/split.js@1.6.0';
+  const container = document.currentScript.previousElementSibling;
+  if (container) {
+    const left  = container.querySelector('.fullshow-left');
+    const right = container.querySelector('.fullshow-right');
+    if (left && right) {
+      Split([left, right], {
+        sizes: [50, 50],
+        minSize: 0,
+        gutterSize: 24,   // equals the two removed 12px borders
+        snapOffset: 0,
+        cursor: 'col-resize',
+        direction: 'horizontal'
+      });
+    }
+  }
+</script>
