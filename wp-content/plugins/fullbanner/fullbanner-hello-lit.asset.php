1bfd8d76951a54d4153c50216cdb258822572e87
Yasuaki Kudo <yasu@yasuaki.com>
2025-08-26 12:30:48 +0900

    i

--- wp-content/plugins/fullbanner/fullbanner-hello-lit.asset.php
+++
@@ -1,6 +0,0 @@
-<?php
-// file: fullbanner-hello-lit.asset.php
-return array(
-    'dependencies' => array(), // add script handles if needed
-    'version'      => filemtime(__DIR__ . '/fullbanner-hello-lit.js'),
-);
