ccbacbe42682c7fd963d6afb7a0564ef8fc89671
Yasuaki Kudo <yasu@tng.coop>
2025-09-23 17:05:44 +0900

    i

--- e2e/fixtures/test.ts
+++ e2e/fixtures/test.ts
@@ -2,7 +2,8 @@
 import { test as base, expect, mergeTests } from '@playwright/test';
 import { test as wpTest } from './wp';
 import { test as blazorTest } from './common';
-import { test as wpLoginTest } from './wp-login';   // 👈 make sure this path is correct
+import { test as wpLoginTest } from './wp-login';
+import { test as wpContentTest } from './wp-content'; // 👈 add this line
 
-export const test = mergeTests(base, wpTest, blazorTest, wpLoginTest);
+export const test = mergeTests(base, wpTest, blazorTest, wpLoginTest, wpContentTest); // 👈 include it here
 export { expect };
